39a4ede8041e6fcc958201f53f1a837b5aa1f869
mr.valerio <valera.osadchuk@gmail.com>
2024-01-03 13:33:09 +0200

    add taskItemControls component

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ref, computed, inject } from 'vue'
 import { Icon } from '@iconify/vue'
-import AppButton from '@/components/AppButton.vue'
 import EditTaskForm from '@/components/EditTaskForm.vue'
+import TaskItemControls from '@/components/TaskItemControls.vue'
 
 /**
  * injects
@@ -38,15 +38,6 @@ const iconClasses = computed(() => ({
 const editMode = ref(false)
 const handleEditForm = () => {
   editMode.value = false
-  editMenu.value = false
-}
-
-/**
- * edit menu
- */
-const editMenu = ref(false)
-const toggleEditMenu = () => {
-  editMenu.value = !editMenu.value
 }
 
 /**
@@ -77,24 +68,8 @@ const handleTaskClick = () => {
     <span class="text-neutral/50 ml-auto"
       >{{ task.completed }}/<small class="font-normal">{{ task.qty }}</small></span
     >
-    <!-- task menu -->
-    <div class="flex gap-x-2 shrink-0">
-      <div v-if="editMenu" class="flex gap-x-2 transition-all">
-        <!-- button delete task -->
-        <AppButton @click="() => deleteTask(task.id)" text="del" class="btn-sm btn-error" />
-        <!-- button edit task -->
-        <AppButton text="edit" @click="editMode = true" class="btn-sm btn-primary" />
-      </div>
-      <!-- button toggle edit menu -->
-      <AppButton
-        @click="toggleEditMenu"
-        :icon="`${editMenu ? 'ph:x-bold' : 'ph:dots-three-outline-vertical-fill'}`"
-        :class="{
-          'btn-sm btn-square btn-primary opacity-30 hover:opacity-70': true,
-          'opacity-70': editMenu
-        }"
-      />
-    </div>
+    <!-- task controls -->
+    <TaskItemControls @edit="editMode = true" @delete="() => deleteTask(task.id)" />
   </li>
   <EditTaskForm v-else :task="task" @close="handleEditForm" />
 </template>
---
+++ src/components/TaskItemControls.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+import AppButton from '@/components/AppButton.vue'
+
+/**
+ * emits
+ */
+defineEmits({
+  delete: null,
+  edit: null
+})
+
+/**
+ * edit menu
+ */
+const editMenu = ref(false)
+const toggleEditMenu = () => {
+  editMenu.value = !editMenu.value
+}
+</script>
+
+<template>
+  <!-- task item controls -->
+  <div class="flex gap-x-2 shrink-0">
+    <div v-if="editMenu" class="flex gap-x-2 transition-all">
+      <!-- button delete task -->
+      <AppButton @click="$emit('delete')" text="del" class="btn-sm btn-error" />
+      <!-- button edit task -->
+      <AppButton text="edit" @click="$emit('edit')" class="btn-sm btn-primary" />
+    </div>
+    <!-- button toggle edit menu -->
+    <AppButton
+      @click="toggleEditMenu"
+      :icon="`${editMenu ? 'ph:x-bold' : 'ph:dots-three-outline-vertical-fill'}`"
+      :class="{
+        'btn-sm btn-square btn-primary opacity-30 hover:opacity-70': true,
+        'opacity-70': editMenu
+      }"
+    />
+  </div>
+</template>
