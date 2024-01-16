3a3d8cc5315be52ae057b92bddc9293d0a77abe2
mr.valerio <valera.osadchuk@gmail.com>
2024-01-16 21:26:48 +0200

    added dummy timeline

---
+++ src/components/TheTimeline.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <ul class="timeline mt-4">
+    <li>
+      <div class="timeline-middle">
+        <Icon icon="ph:check-circle-fill" class="text-info text-xl" />
+      </div>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+    </li>
+    <li>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+      <div class="timeline-middle">
+        <Icon icon="ph:clock-countdown" />
+      </div>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+    </li>
+    <li>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+      <div class="timeline-middle">
+        <Icon icon="ph:check-circle-fill" class="text-info text-xl" />
+      </div>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+    </li>
+    <li>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+      <div class="timeline-middle">
+        <Icon icon="ph:clock-countdown" />
+      </div>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+    </li>
+    <li>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+      <div class="timeline-middle">
+        <Icon icon="ph:check-circle-fill" class="text-info text-xl" />
+      </div>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+    </li>
+    <li>
+      <hr class="min-w-2 max-h-[2px] bg-primary-content/15" />
+      <div class="timeline-middle">
+        <Icon icon="ph:clock-countdown-bold" />
+      </div>
+    </li>
+  </ul>
+</template>
