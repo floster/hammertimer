749e24e55ce12cad313e09c2c44f30c925dbb2eb
mr.valerio <valera.osadchuk@gmail.com>
2024-01-19 10:56:51 +0200

    added TaskActiveSection component

---
+++ src/components/TaskActiveSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+/*
+  import tasks store
+*/
+import { useTasksStore } from '@/stores/tasks'
+const tasks = useTasksStore()
+
+/*
+  import timer store
+*/
+import { usePomodoroStore } from '@/stores/pomodoro'
+const pomodoro = usePomodoroStore()
+</script>
+
+<template>
+  <h2 v-if="tasks.activeTaskTitle && pomodoro.isCurrentModePomodoro" class="text-center text-sm">
+    {{ tasks.activeTaskTitle }}
+  </h2>
+</template>
--- src/components/TimerView.vue
+++ src/components/TimerView.vue
@@ -2,33 +2,20 @@
 import AppButton from '@/components/AppButton.vue'
 import TheTimer from '@/components/TheTimer.vue'
 import TimerModesSwitcher from '@/components/TimerModesSwitcher.vue'
-
-/*
-  import tasks store
-*/
-import { useTasksStore } from '@/stores/tasks'
-const tasks = useTasksStore()
+import TaskActiveSection from '@/components/TaskActiveSection.vue'
 
 /*
   import timer store
 */
 import { useTimerStore } from '@/stores/timer'
 const timer = useTimerStore()
-
-/*
-  import timer store
-*/
-import { usePomodoroStore } from '@/stores/pomodoro'
-const pomodoro = usePomodoroStore()
 </script>
 
 <template>
   <section
     class="flex flex-col items-center justify-center gap-y-10 w-full mt-10 px-2 pt-4 pb-6 md:p-6 md:pb-8 bg-white/10 rounded-lg shadow-md">
     <TimerModesSwitcher />
-    <h2 v-if="tasks.activeTaskTitle && pomodoro.isCurrentModePomodoro" class="text-center text-sm">
-      {{ tasks.activeTaskTitle }}
-    </h2>
+    <TaskActiveSection />
     <TheTimer />
     <div class="flex items-center gap-x-2">
       <!-- reset current button -->
@@ -39,7 +26,7 @@ const pomodoro = usePomodoroStore()
         @click="timer.reset()" />
       <!-- start/pause button -->
       <AppButton
-        :icon="timer.isStarted ? 'ph:pause-fill' : 'ph:play-fill'"
+        :icon="timer.isStarted && !timer.isPaused ? 'ph:pause-fill' : 'ph:play-fill'"
         class="btn-warning btn-lg px-16"
         @click="timer.toggle()" />
       <!-- force finish current button -->
